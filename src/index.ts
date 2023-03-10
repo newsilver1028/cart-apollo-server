329b8c87cbb257760d87d599c15f1d3bd0b87705
gaeun <gaeun@prestolabs.io>
2023-03-10 22:52:01 +0900

    FIX: port number

--- src/index.ts
+++ src/index.ts
@@ -111,8 +111,13 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-})
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+// })
 
-console.log(`🚀  Server ready at: ${url}`)
+const startApolloServer = async (server) => {
+  const { url } = await server.listen({ port: 4000 })
+  console.log(`🚀 Server ready at ${url}`)
+}
+
+startApolloServer(server)
